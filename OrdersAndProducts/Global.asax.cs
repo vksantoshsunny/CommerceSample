3d00e35bdd566b2b40d9009e41d4a9a0cb575545
vksantoshsunny <vksantoshsunny@gmail.com>
2017-11-21 20:06:17 +1300

    removed unused code

--- OrdersAndProducts/Global.asax.cs
+++ OrdersAndProducts/Global.asax.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace OrdersAndProducts
 {
-    public class MvcApplication : System.Web.HttpApplication
+	public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
