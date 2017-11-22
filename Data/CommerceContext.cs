c4fce9fb48dab27bc4dc9184edfe19f66617353a
vksantoshsunny <vksantoshsunny@gmail.com>
2017-11-22 18:56:32 +1300

    added nunit and connection string update

--- Data/CommerceContext.cs
+++ Data/CommerceContext.cs
@@ -11,7 +11,7 @@ namespace Data
 
 		public CommerceContext() : base("Name=ECommerceEntities")
 		{
-
+			 
 		}
 	}
 }
--- OrdersAndProducts.Tests/ServiceTest.cs
+++ OrdersAndProducts.Tests/ServiceTest.cs
@@ -1,32 +1,37 @@
 ﻿using Data;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using Services;
-using Services.DTOs;
+using NUnit.Framework;
 using Services.Interfaces;
 using Services.Repository;
 using Services.Service;
 using System.Collections.Generic;
 
+
 namespace OrdersAndProducts.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class ServiceTest
     {
+		protected List<Product> products;
 
-        [TestMethod]
+		[OneTimeSetUp]
+		public void FixtureSetUp()
+		{
+			products = new List<Product>(){
+											 new Product(){Description="P1",Id=1,Name="p1Name"},
+											 new Product(){Description="P2",Id=2,Name="p2Name"},
+											 new Product(){Description="P3",Id=3,Name="p3Name"},
+											 new Product(){Description="P4",Id=4,Name="p4Name"},
+											 new Product(){Description="P5",Id=5,Name="p5Name"},
+											 new Product(){Description="P6",Id=6,Name="p6Name"},
+
+										 };
+		}
+
+		[Test]
         public void Can_Get_All_Product_Service()
         {
-            List<Product> products = new List<Product>()
-                                         {
-                                             new Product(){Description="P1",Id=1,Name="p1Name"},
-                                             new Product(){Description="P2",Id=2,Name="p2Name"},
-                                             new Product(){Description="P3",Id=3,Name="p3Name"},
-                                             new Product(){Description="P4",Id=4,Name="p4Name"},
-                                             new Product(){Description="P5",Id=5,Name="p5Name"},
-                                             new Product(){Description="P6",Id=6,Name="p6Name"},
-
-                                         };
+           
 
             Mock<IProductRepository> mockRepository = new Mock<IProductRepository>();
             mockRepository.Setup(m => m.GetAll()).Returns(products);
@@ -37,7 +42,7 @@ namespace OrdersAndProducts.Tests
             Service service = new Service(mockUOW.Object);
 
             var result = service.GetAllProducts();
-            Assert.AreEqual(products.Count, result.Count);
+			Assert.AreEqual(products.Count, result.Count);
 
 
         }
